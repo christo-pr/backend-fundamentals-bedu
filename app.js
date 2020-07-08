14b5bc2940938250eba6135ddcb8925638eff6e0
Christofer Flores <cristofer.flort@gmail.com>
2020-07-08 15:54:49 -0500

    Add the middleware to handle protected routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express")
 var path = require("path")
 var cookieParser = require("cookie-parser")
 var logger = require("morgan")
+const passport = require("passport")
 require("dotenv").config()
 
 // Auth
@@ -9,6 +10,9 @@ require("./passport")
 
 // Routes
 var authRouter = require("./routes/auth")
+
+// Users
+var usersRouter = require("./routes/users")
 
 var app = express()
 
@@ -19,5 +23,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/auth", authRouter)
+app.use("/users", passport.authenticate("jwt", { session: false }), usersRouter)
 
 module.exports = app
--- passport.js
+++ passport.js
@@ -1,7 +1,12 @@
 var passport = require("passport")
+var passportJWT = require("passport-jwt")
+
 var models = require("./db/models")
 var LocalStrategy = require("passport-local").Strategy
+var JWTStrategy = passportJWT.Strategy
+var ExtractJWT = passportJWT.ExtractJwt
 
+//  Login using local strategy
 passport.use(
   new LocalStrategy(
     {
@@ -16,6 +21,28 @@ passport.use(
           return cb(null, user)
         })
         .catch((err) => {
+          return cb(err)
+        })
+    }
+  )
+)
+
+// Middleware to protect routes
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    function (jwtPayload, cb) {
+      console.log("jwtPayload", jwtPayload)
+      return models.User.find(jwtPayload.id)
+        .then((user) => {
+          console.log("user", user)
+          return cb(null, user)
+        })
+        .catch((err) => {
+          console.log("err", err)
           return cb(err)
         })
     }
