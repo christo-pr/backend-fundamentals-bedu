45048b794749e2997283bf66244b207d233a946d
Christofer Flores <cristofer.flort@gmail.com>
2020-07-10 09:36:59 -0500

    Create the route for listings and the seeder

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ require("./passport")
 
 // Routes
 var authRouter = require("./routes/auth")
-
 // Users
 var usersRouter = require("./routes/users")
+// Listings
+var listingsRouter = require("./routes/listings")
 
 var app = express()
 
@@ -24,5 +25,10 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/auth", authRouter)
 app.use("/users", passport.authenticate("jwt", { session: false }), usersRouter)
+app.use(
+  "/listings",
+  passport.authenticate("jwt", { session: false }),
+  listingsRouter
+)
 
 module.exports = app
---
+++ db/seeders/20200710132346-Listings.js
@@ -0,0 +1,39 @@
+"use strict"
+var faker = require("faker")
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    const listings = new Array(5).fill().map(() => ({
+      userId: 1,
+      name: faker.company.companyName(),
+      description: faker.lorem.lines(2),
+      address: `${faker.address.streetAddress}, ${faker.address.country()}`,
+      price: faker.finance.amount() * 10,
+      maxAllow: Math.floor(Math.random() * (5 - 1) + 1),
+      payDate: `${Math.floor(Math.random() * (31 - 1) + 1)} de cada mes`,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }))
+    await queryInterface.bulkInsert("listings", listings, {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("listings", null, {})
+  },
+}
