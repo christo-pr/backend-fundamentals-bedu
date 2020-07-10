e60f26e0a18e1c8c996f234bb7517bf1840f8b0e
Christofer Flores <cristofer.flort@gmail.com>
2020-07-10 09:34:27 -0500

    Fix error of database connection, silent connect to other, so no data was displayed

--- db/config.js
+++ db/config.js
@@ -2,10 +2,10 @@ require("dotenv").config()
 
 module.exports = {
   development: {
-    // url: process.env.DATABASE_URL,
-    // dialect: "postgres",
-    dialect: "sqlite",
-    storage: "data/dev.sqlite3",
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    // dialect: "sqlite",
+    // storage: "data/dev.sqlite3",
   },
   test: {
     dialect: "sqlite",
