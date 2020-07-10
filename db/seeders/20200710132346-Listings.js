6bd7740601ac61505b2e5ef641a3c0aff3eba40d
Christofer Flores <cristofer.flort@gmail.com>
2020-07-10 11:47:35 -0500

    Fix a typo in the listings seed and create a npm script to revert all the migrations

--- db/seeders/20200710132346-Listings.js
+++ db/seeders/20200710132346-Listings.js
@@ -17,7 +17,7 @@ module.exports = {
       userId: 1,
       name: faker.company.companyName(),
       description: faker.lorem.lines(2),
-      address: `${faker.address.streetAddress}, ${faker.address.country()}`,
+      address: `${faker.address.streetAddress()}, ${faker.address.country()}`,
       price: faker.finance.amount() * 10,
       maxAllow: Math.floor(Math.random() * (5 - 1) + 1),
       payDate: `${Math.floor(Math.random() * (31 - 1) + 1)} de cada mes`,
