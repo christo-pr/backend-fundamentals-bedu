7e0364fd4c27208a50c8a2bb6ca4c36038d7a869
Christofer Flores <cristofer.flort@gmail.com>
2020-07-15 16:54:50 -0500

    Listings CRUD finished

--- routes/listings.js
+++ routes/listings.js
@@ -7,4 +7,70 @@ router.get("/", function (req, res, next) {
   return db.Listing.findAll().then((listings) => res.json(listings))
 })
 
+/* GET users listing. */
+router.get("/:id", function (req, res, next) {
+  var listingId = req.params.id
+  return db.Listing.findByPk(listingId).then((listing) => res.json(listing))
+})
+
+/* POST users listing. */
+router.post("/", function (req, res, next) {
+  var { name, description, address, price, maxAllow, payDate } = req.body
+
+  var newListing = {
+    name,
+    description,
+    address,
+    price,
+    maxAllow,
+    payDate,
+    userId: req.user.id,
+  }
+
+  db.Listing.create(newListing)
+    .then(function (listing) {
+      res.json({ success: true, data: listing })
+    })
+    .catch(function (error) {
+      res.status(500).json({ error: error })
+    })
+})
+
+/* PUT users listing. */
+router.put("/:id", function (req, res, next) {
+  var listingId = req.params.id
+  var { name, description, address, price, maxAllow, payDate } = req.body
+
+  var updatedListing = {
+    id: listingId,
+    name,
+    description,
+    address,
+    price,
+    maxAllow,
+    payDate,
+  }
+
+  db.Listing.update(updatedListing, { where: { id: listingId } })
+    .then(function (listing) {
+      res.json({ success: true, data: updatedListing })
+    })
+    .catch(function (error) {
+      res.status(500).json({ error: error })
+    })
+})
+
+/* DELETE users listing. */
+router.delete("/:id", function (req, res, next) {
+  var listingId = req.params.id
+
+  db.Listing.destroy({ where: { id: listingId } })
+    .then(function () {
+      res.json({ success: true })
+    })
+    .catch(function (error) {
+      res.status(500).json({ error: error })
+    })
+})
+
 module.exports = router
